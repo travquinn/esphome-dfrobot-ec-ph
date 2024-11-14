60cd25da6798e4e2d93be6eb809e6323ae8a2fc0
travquinn <53845837+travquinn@users.noreply.github.com>
2024-11-14 19:54:31 +1100

    Create dfrobot_ph_sensor.h

---
+++ components/dfrobot_ph_sensor/dfrobot_ph_sensor.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/sensor/sensor.h"
+#include "esphome/components/i2c/i2c.h"
+#include "DFRobot_ESP_PH_WITH_ADC.h"
+#include "Adafruit_ADS1015.h"
+
+namespace esphome {
+namespace dfrobot_ph_sensor {
+
+class DFRobotPHSensor : public sensor::Sensor, public PollingComponent, public i2c::I2CDevice {
+ public:
+  void set_temperature_sensor(sensor::Sensor *temp_sensor) { temp_sensor_ = temp_sensor; }
+  void set_adc_channel(int channel) { adc_channel_ = channel; }
+
+  void setup() override;
+  void update() override;
+  void dump_config() override;
+
+ protected:
+  sensor::Sensor *temp_sensor_{nullptr};
+  int adc_channel_{0};
+  DFRobot_ESP_PH_WITH_ADC ph_;
+  Adafruit_ADS1115 ads_;
+};
+
+}  // namespace dfrobot_ph_sensor
+}  // namespace esphome
