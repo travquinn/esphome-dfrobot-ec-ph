0533d20544e1cb6ec9d9cb9dcd42f2ee9dddbcc4
travquinn <53845837+travquinn@users.noreply.github.com>
2024-11-14 20:27:47 +1100

    Update dfrobot_ph_sensor.h

--- components/dfrobot_ph_sensor/dfrobot_ph_sensor.h
+++ components/dfrobot_ph_sensor/dfrobot_ph_sensor.h
@@ -11,20 +11,19 @@ namespace dfrobot_ph_sensor {
 
 class DFRobotPHSensor : public sensor::Sensor, public PollingComponent, public i2c::I2CDevice {
  public:
-   void set_temperature_sensor(sensor::Sensor *temp_sensor) { temp_sensor_ = temp_sensor; }
-   void set_adc_channel(int channel) { adc_channel_ = channel; }
-   void set_ads1115_id(const std::string &id) { ads1115_id_ = id; }
+  void set_temperature_sensor(sensor::Sensor *temp_sensor) { temp_sensor_ = temp_sensor; }
+  void set_adc_channel(int channel) { adc_channel_ = channel; }
+  void set_ads1115(Adafruit_ADS1115 *ads) { ads_ = ads; } // Add this line
 
-   void setup() override;
-   void update() override;
-   void dump_config() override;
+  void setup() override;
+  void update() override;
+  void dump_config() override;
 
  protected:
-   sensor::Sensor *temp_sensor_{nullptr};
-   int adc_channel_{0};
-   std::string ads1115_id_;  // Store ADS1115 ID
-   DFRobot_ESP_PH_WITH_ADC ph_;
-   Adafruit_ADS1115 ads_;
+  sensor::Sensor *temp_sensor_{nullptr};
+  int adc_channel_{0};
+  DFRobot_ESP_PH_WITH_ADC ph_;
+  Adafruit_ADS1115 *ads_; // Change this to a pointer
 };
 
 }  // namespace dfrobot_ph_sensor
