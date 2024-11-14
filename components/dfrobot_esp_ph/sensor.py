c55b7c4e338287095648da4f3ea5dddb50856fd4
travquinn <53845837+travquinn@users.noreply.github.com>
2024-11-14 19:16:23 +1100

    Update sensor.py

--- components/dfrobot_esp_ph/sensor.py
+++ components/dfrobot_esp_ph/sensor.py
@@ -8,10 +8,7 @@ DEPENDENCIES = ['dfrobot_esp_ph']
 dfrobot_esp_ph_ns = cg.esphome_ns.namespace('dfrobot_esp_ph')
 DFRobotESPPH = dfrobot_esp_ph_ns.class_('DFRobotESPPH', sensor.Sensor, cg.PollingComponent)
 
-CONFIG_SCHEMA = sensor.sensor_schema().extend({
-    cv.GenerateID(): cv.declare_id(DFRobotESPPH),
-})
+CONFIG_SCHEMA = cv.Schema({})
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await sensor.register_sensor(var, config)
+    pass
