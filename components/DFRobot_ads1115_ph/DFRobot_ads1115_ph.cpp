974e29c381aefd1f1b646fc07f5bc3617d03d30a
travquinn <53845837+travquinn@users.noreply.github.com>
2024-11-14 18:52:01 +1100

    Update DFRobot_ads1115_ph.cpp

--- components/DFRobot_ads1115_ph/DFRobot_ads1115_ph.cpp
+++ components/DFRobot_ads1115_ph/DFRobot_ads1115_ph.cpp
@@ -1,80 +1,44 @@
 #include "DFRobot_ads1115_ph.h"
 #include "esphome/core/log.h"
 
-static const char * TAG ="PH Sensor";
+namespace esphome {
+namespace dfrobot_ads1115_ph {
 
+static const char *const TAG = "dfrobot_ads1115_ph";
 
-namespace esphome{
- namespace dfrobot_ads1115_ph_{
-    static int index = 0 ;
-    void DFRobotADS1115PH::dump_config(){
-       ESP_LOGCONFIG(TAG, "PH Sensor Setting UP ");
-       ESP_LOGCONFIG(TAG, "PH Sensor Connected to Channel : %d ", this->_channel);
-       ESP_LOGCONFIG(TAG, "PH Sensor Acid Voltage Calibration is : %.2f ", this->_acidVoltage);
-       ESP_LOGCONFIG(TAG, "PH Sensor Neutral Voltage Calibration is : %.2f ", this->_neutralVoltage);
-    }
+void CalibrationSwitch::write_state(bool state) {
+  this->publish_state(state);
+}
 
-    void DFRobotADS1115PH::setup(){
-        if(!(this->_neutralVoltage> PH_8_VOLTAGE) && (this->_neutralVoltage < PH_6_VOLTAGE)){
-            // buffer solution:7.0 is not valid
-            ESP_LOGE(TAG, "Neutral Voltage(Solution:7.0) Configuration Error");
-            ESP_LOGE(TAG, "Check the Neutral Voltage Calibration");
-            ESP_LOGE(TAG, "A valid calibration is range is [%d ,  %d]", PH_8_VOLTAGE, PH_6_VOLTAGE);
-        }
+void DFRobotADS1115PH::setup() {
+  this->calibration_switch_->set_parent(this);
+}
 
-        if(!(this->_acidVoltage > PH_5_VOLTAGE) && (this->_acidVoltage < PH_3_VOLTAGE)){
-            //buffer solution:4.0
-            ESP_LOGE(TAG, "Acid Voltage(Solution:4.0) Configuration Error");
-            ESP_LOGE(TAG, "Check the Acid Voltage Calibration");
-            ESP_LOGE(TAG, "A valid calibration is range is [%d ,  %d]", PH_5_VOLTAGE, PH_3_VOLTAGE);
-            
-        }
-    }
+void DFRobotADS1115PH::update() {
+  float voltage = this->ads1115_->read_voltage(this->channel_);
+  
+  if (this->calibration_switch_->state) {
+    ESP_LOGD(TAG, "Calibration mode: Voltage = %.2f", voltage);
+    return;
+  }
 
+  this->ph_value_ = this->read_ph(voltage);
+  this->ph_sensor_->publish_state(this->ph_value_);
+}
 
-    void DFRobotADS1115PH::update(){
-        //float  voltage = analogRead(this->_pin) / ESPADC * ESPVOLTAGE; // read the voltage
-        //float  voltage = getAnalogRead() / ESPADC * ESPVOLTAGE; // read the voltage
-        float  voltage =0;
-        if(!_ads1115_adc->is_failed()){
-           voltage = _ads1115_adc->analogRead(_channel);
-        }else{
-            ESP_LOGE(TAG, "The ADS1115 Module is not correctly initialized");
-            return ;
-        }
-        
-        voltage = voltage*1000;
-        if(this->_calibration_mode_switch->state){
-          ESP_LOGD(TAG, "Calibration mode of the PH Sensor ");
-          ESP_LOGD(TAG, "Use and Acid or Neutral solution for calibration");
-          ESP_LOGD(TAG, "Copy the following Voltage corresponding to your Acid or Neutral Voltage Calibration");
-          ESP_LOGD(TAG, "The Calibration voltage is : %.2f", voltage);
-          return ;  
-        }
-        //float  voltage = analogRead(this->_pin) / ESPADC * ESPVOLTAGE; // read the voltage
-        float phValue = this->readPH(voltage, this->_temperature);
-        this->_ph_sensor->publish_state(phValue) ;
-    }
+void DFRobotADS1115PH::dump_config() {
+  ESP_LOGCONFIG(TAG, "DFRobot ADS1115 pH Sensor:");
+  ESP_LOGCONFIG(TAG, "  Channel: %d", this->channel_);
+  ESP_LOGCONFIG(TAG, "  Acid Voltage: %.2f", this->acid_voltage_);
+  ESP_LOGCONFIG(TAG, "  Neutral Voltage: %.2f", this->neutral_voltage_);
+  ESP_LOGCONFIG(TAG, "  Temperature: %.2f", this->temperature_);
+}
 
-    float DFRobotADS1115PH::readPH(float voltage, float temperature){
-        float slope = (7.0 - 4.0) / ((this->_neutralVoltage - 1500.0) / 3.0 - (this->_acidVoltage - 1500.0) / 3.0); // two point: (_neutralVoltage,7.0),(_acidVoltage,4.0)
-        float intercept = 7.0 - slope * (this->_neutralVoltage - 1500.0) / 3.0;
-        this->_phValue = slope * (voltage - 1500.0) / 3.0 + intercept; //y = k*x + b
-        return this->_phValue;
-    }
+float DFRobotADS1115PH::read_ph(float voltage) {
+  float slope = (7.0 - 4.0) / ((this->neutral_voltage_ - 1500.0) / 3.0 - (this->acid_voltage_ - 1500.0) / 3.0);
+  float intercept = 7.0 - slope * (this->neutral_voltage_ - 1500.0) / 3.0;
+  return slope * (voltage - 1500.0) / 3.0 + intercept;
+}
 
-
-    
-    void DigitalSwitch::setup(){
-       this->pinState = true ;
-    }
-    void DigitalSwitch::write_state(bool state){
-        this->pinState = state ;
-        this->publish_state(state);
-        
-    }
-    void DigitalSwitch::dump_config(){
-        
-    }
- };
-};
+}  // namespace dfrobot_ads1115_ph
+}  // namespace esphome
