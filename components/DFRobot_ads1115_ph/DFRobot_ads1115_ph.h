a7ee32d43c54439d867cef2aa043cc1a7d10ff47
travquinn <53845837+travquinn@users.noreply.github.com>
2024-11-14 18:50:41 +1100

    Update DFRobot_ads1115_ph.h

--- components/DFRobot_ads1115_ph/DFRobot_ads1115_ph.h
+++ components/DFRobot_ads1115_ph/DFRobot_ads1115_ph.h
@@ -1,68 +1,48 @@
-#ifndef __DFRobot_ESP_PH__
-#define __DFRobot_ESP_PH__
-
+#pragma once
 
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/switch/switch.h"
 #include "esphome/components/ads1115_adc/ads1115_adc.h"
-#include<Arduino.h>
 
-#define PH_8_VOLTAGE 1122
-#define PH_6_VOLTAGE 1478
-#define PH_5_VOLTAGE 1654
-#define PH_3_VOLTAGE 2010
+namespace esphome {
+namespace dfrobot_ads1115_ph {
 
-#define ESPADC 4096.0   //the esp Analog Digital Convertion value
-#define ESPVOLTAGE 3300 //the esp voltage supply value
-
-namespace esphome{
-    namespace dfrobot_ads1115_ph_ {
-         class DigitalSwitch;
-         class DFRobotADS1115PH : public PollingComponent{
-            protected:
-            uint8_t _channel ;
-            float _phValue;
-            float _acidVoltage;
-            float _neutralVoltage;
-            float _voltage;
-            float _temperature;
-         
-            sensor::Sensor *_ph_sensor{nullptr} ;
-            dfrobot_ads1115_ph_::DigitalSwitch *_calibration_mode_switch{nullptr} ;
-            ads1115_adc_::ADS1115_ADC * _ads1115_adc;
-            float getAnalogRead();
-            public: 
-            void setup()override ;
-            void update() override ;
-            void dump_config() override;
-            float get_setup_priority() const override { return esphome::setup_priority::LATE-100; }
-            float readPH(float voltage, float temperature); // voltage to pH value, with temperature compensation
-            void set_channel (uint8_t ch){this->_channel = ch ;}
-            void set_temperature(float t){this->_temperature = t ;}
-            void set_cal_acidVoltage(float v){this->_acidVoltage = v ;}
-            void set_cal_neutralVoltage(float v){this->_neutralVoltage = v ;}
-            void set_calibration_mode_switch(dfrobot_ads1115_ph_::DigitalSwitch  *sw){this->_calibration_mode_switch = sw ;}
-            void set_ph_sensor(sensor::Sensor *s){this->_ph_sensor = s ;}
-            void set_ads1115(ads1115_adc_::ADS1115_ADC *ads){this->_ads1115_adc = ads ;}
-         };
-
-
-         class DigitalSwitch: public switch_::Switch, public Component{
-            public:
-            DigitalSwitch(){
-
-            }
-
-
-            void setup() override;
-            void write_state(bool state) override;
-            void dump_config() override;
-
-            bool pinState ;
-        };
-    };
-
+class CalibrationSwitch : public switch_::Switch, public Component {
+ public:
+  void set_parent(Component *parent) { parent_ = parent; }
+  void write_state(bool state) override;
+ protected:
+  Component *parent_{nullptr};
 };
 
-#endif
+class DFRobotADS1115PH : public PollingComponent {
+ public:
+  void set_ads1115(ads1115_adc::ADS1115_ADC *ads) { ads1115_ = ads; }
+  void set_channel(uint8_t channel) { channel_ = channel; }
+  void set_acid_voltage(float voltage) { acid_voltage_ = voltage; }
+  void set_neutral_voltage(float voltage) { neutral_voltage_ = voltage; }
+  void set_temperature(float temp) { temperature_ = temp; }
+  void set_ph_sensor(sensor::Sensor *sens) { ph_sensor_ = sens; }
+  void set_calibration_switch(CalibrationSwitch *sw) { calibration_switch_ = sw; }
+
+  void setup() override;
+  void update() override;
+  void dump_config() override;
+  float get_setup_priority() const override { return setup_priority::DATA; }
+
+ protected:
+  ads1115_adc::ADS1115_ADC *ads1115_{nullptr};
+  sensor::Sensor *ph_sensor_{nullptr};
+  CalibrationSwitch *calibration_switch_{nullptr};
+  uint8_t channel_{0};
+  float acid_voltage_{0};
+  float neutral_voltage_{0};
+  float temperature_{25};
+  float ph_value_{0};
+
+  float read_ph(float voltage);
+};
+
+}  // namespace dfrobot_ads1115_ph
+}  // namespace esphome
