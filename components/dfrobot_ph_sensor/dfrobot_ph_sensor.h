3426347def09971b9e75b71e8a58ad90a2282d85
travquinn <53845837+travquinn@users.noreply.github.com>
2024-11-14 20:40:58 +1100

    Update dfrobot_ph_sensor.h

--- components/dfrobot_ph_sensor/dfrobot_ph_sensor.h
+++ components/dfrobot_ph_sensor/dfrobot_ph_sensor.h
@@ -5,6 +5,7 @@
 #include "esphome/components/i2c/i2c.h"
 #include "DFRobot_ESP_PH_WITH_ADC.h"
 #include <Adafruit_ADS1X15.h>
+#include "esphome/components/ads1115/ads1115.h" // Include the ADS1115 component
 
 namespace esphome {
 namespace dfrobot_ph_sensor {
@@ -13,7 +14,7 @@ class DFRobotPHSensor : public sensor::Sensor, public PollingComponent, public i
  public:
   void set_temperature_sensor(sensor::Sensor *temp_sensor) { temp_sensor_ = temp_sensor; }
   void set_adc_channel(int channel) { adc_channel_ = channel; }
-  void set_ads1115(Adafruit_ADS1115 *ads) { ads_ = ads; } // Change this line
+  void set_ads1115(ADS1115Component *ads) { ads_ = ads; } // Change this line
 
   void setup() override;
   void update() override;
@@ -23,7 +24,7 @@ class DFRobotPHSensor : public sensor::Sensor, public PollingComponent, public i
   sensor::Sensor *temp_sensor_{nullptr};
   int adc_channel_{0};
   DFRobot_ESP_PH_WITH_ADC ph_;
-  Adafruit_ADS1115 *ads_; // Change this to a pointer
+  ADS1115Component *ads_; // Change this to point to ADS1115Component
 };
 
 }  // namespace dfrobot_ph_sensor
