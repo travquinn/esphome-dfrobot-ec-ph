15a0dad622fe614f79a901b43538759bfc3319cd
travquinn <53845837+travquinn@users.noreply.github.com>
2024-11-15 20:51:36 +1100

    Update sensor.py

--- components/dfrobot_ph_sensor/sensor.py
+++ components/dfrobot_ph_sensor/sensor.py
@@ -1,5 +1,4 @@
-# /path/to/your/homeassistant/config/esphome/components/ph_sensor/sensor.py
-import math
+# sensor.py
 from homeassistant.components.sensor import PLATFORM_SCHEMA, SensorEntity
 import homeassistant.helpers.config_validation as cv
 from homeassistant.const import DEVICE_CLASS_SENSOR, STATE_UNKNOWN
@@ -8,60 +7,39 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     cv.Required("name"): cv.string,
     cv.Optional("temperature_sensor", default=None): cv.entity_id,
     cv.Required("adc_channel"): cv.positive_int,
-    cv.Required("i2c_id"): cv.use_id(busio.I2C),
+    cv.Required("i2c_id"): cv.use_id(i2c.I2CComponent),
 })
 
-def setup_platform(hass, config, add_entities, discovery_info):
-    name = config.get("name")
-    temperature_sensor = config.get("temperature_sensor", None)
-    adc_channel = config.get("adc_channel")
-    i2c_id = config.get("i2c_id")
-
-    # Initialize the pH sensor
-    ph_sensor = PhSensor(name, temperature_sensor, adc_channel, i2c_id)
-
-    add_entities([ph_sensor])
-
-class PhSensor(SensorEntity):
-    def __init__(self, name, temperature_sensor, adc_channel, i2c_id):
+class DFRobotPHSensor(SensorEntity):
+    def __init__(self, name, adc_channel, i2c_id, temperature_sensor):
         self._name = name
-        self._temperature_sensor = temperature_sensor
         self._adc_channel = adc_channel
         self._i2c_id = i2c_id
-        self._state = STATE_UNKNOWN
+        self._temperature_sensor = temperature_sensor
+        self._state = None
 
     @property
     def name(self):
         return self._name
 
     @property
-    def state(self):
-        return self._state
-
-    @property
     def device_class(self):
         return DEVICE_CLASS_SENSOR
 
-    async def async_update(self):
-        # Read the raw value of the ADC channel.
-        adc_value = self._i2c_id.read_analog_in_value(self._adc_channel)
-        # Convert the raw value to a voltage (assuming 3.3V reference).
-        voltage = adc_value * (3.3 / 65536)
-        
-        # Simulate reading temperature from another sensor (you should replace this with actual code)
-        if self._temperature_sensor:
-            temperature_sensor_entity = hass.states.get(self._temperature_sensor)
-            if temperature_sensor_entity and temperature_sensor_entity.state is not None:
-                temperature = float(temperature_sensor_entity.state)
-            else:
-                temperature = STATE_UNKNOWN
-        else:
-            temperature = STATE_UNKNOWN
-        
-        # Calculate pH based on the voltage (this is an example calculation, you might need to adjust it).
-        if adc_value != 0 and temperature != STATE_UNKNOWN:
-            # Example conversion formula for pH from voltage and temperature
-            pH = -2.0798 * math.log(voltage) + 12.47
-            self._state = pH
-        else:
-            self._state = STATE_UNKNOWN
+    @property
+    def state(self):
+        return self._state
+
+    async def update(self):
+        # Simulate fetching data from the sensor
+        adc_value = await self._i2c_id.read_adc(self._adc_channel)
+        temperature = await self._temperature_sensor.state
+        self._state = calculate_ph(adc_value, temperature)
+
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    i2c = i2c.I2CComponent.from_config(config, hass)
+    adc_channel = config.get("adc_channel")
+    temperature_sensor = sensor.SensorEntity.from_config(config.get("temperature_sensor"), hass)
+    
+    ph_sensor = DFRobotPHSensor(config.get("name"), adc_channel, i2c, temperature_sensor)
+    add_entities([ph_sensor])
