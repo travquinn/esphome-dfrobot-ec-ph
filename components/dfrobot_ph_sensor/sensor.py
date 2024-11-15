ab2ba68c8a9a39098e1e2a9d8b933c5144bc44e3
travquinn <53845837+travquinn@users.noreply.github.com>
2024-11-15 20:45:59 +1100

    Update sensor.py

--- components/dfrobot_ph_sensor/sensor.py
+++ components/dfrobot_ph_sensor/sensor.py
@@ -1,11 +1,8 @@
 # /path/to/your/homeassistant/config/esphome/components/ph_sensor/sensor.py
-import time
-import board
-import busio
-import adafruit_ads1x15.ads1115 as ADS
-from adafruit_ads1x15.analog_in import AnalogIn
+import math
 from homeassistant.components.sensor import PLATFORM_SCHEMA, SensorEntity
 import homeassistant.helpers.config_validation as cv
+from homeassistant.const import DEVICE_CLASS_SENSOR, STATE_UNKNOWN
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     cv.Required("name"): cv.string,
@@ -20,20 +17,18 @@ def setup_platform(hass, config, add_entities, discovery_info):
     adc_channel = config.get("adc_channel")
     i2c_id = config.get("i2c_id")
 
-    # Initialize the ADS1115 ADC
-    ads = ADS.ADS1115(i2c_id)
+    # Initialize the pH sensor
+    ph_sensor = PhSensor(name, temperature_sensor, adc_channel, i2c_id)
 
-    # Create an analog input channel.
-    channel = AnalogIn(ads, adc_channel)
-
-    add_entities([PhSensor(name, temperature_sensor, channel)])
+    add_entities([ph_sensor])
 
 class PhSensor(SensorEntity):
-    def __init__(self, name, temperature_sensor, channel):
+    def __init__(self, name, temperature_sensor, adc_channel, i2c_id):
         self._name = name
         self._temperature_sensor = temperature_sensor
-        self._channel = channel
-        self._state = None
+        self._adc_channel = adc_channel
+        self._i2c_id = i2c_id
+        self._state = STATE_UNKNOWN
 
     @property
     def name(self):
@@ -43,11 +38,30 @@ class PhSensor(SensorEntity):
     def state(self):
         return self._state
 
+    @property
+    def device_class(self):
+        return DEVICE_CLASS_SENSOR
+
     async def async_update(self):
         # Read the raw value of the ADC channel.
-        raw_value = self._channel.value
+        adc_value = self._i2c_id.read_analog_in_value(self._adc_channel)
         # Convert the raw value to a voltage (assuming 3.3V reference).
-        voltage = raw_value * (3.3 / 65536)
+        voltage = adc_value * (3.3 / 65536)
+        
+        # Simulate reading temperature from another sensor (you should replace this with actual code)
+        if self._temperature_sensor:
+            temperature_sensor_entity = hass.states.get(self._temperature_sensor)
+            if temperature_sensor_entity and temperature_sensor_entity.state is not None:
+                temperature = float(temperature_sensor_entity.state)
+            else:
+                temperature = STATE_UNKNOWN
+        else:
+            temperature = STATE_UNKNOWN
+        
         # Calculate pH based on the voltage (this is an example calculation, you might need to adjust it).
-        pH = -2.0798 * math.log(voltage) + 12.47
-        self._state = pH
+        if adc_value != 0 and temperature != STATE_UNKNOWN:
+            # Example conversion formula for pH from voltage and temperature
+            pH = -2.0798 * math.log(voltage) + 12.47
+            self._state = pH
+        else:
+            self._state = STATE_UNKNOWN
