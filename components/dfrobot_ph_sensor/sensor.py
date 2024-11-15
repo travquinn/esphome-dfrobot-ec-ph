ffae67d31c88d756ea175a7d9278e7696efd246f
travquinn <53845837+travquinn@users.noreply.github.com>
2024-11-15 20:24:25 +1100

    Update sensor.py

--- components/dfrobot_ph_sensor/sensor.py
+++ components/dfrobot_ph_sensor/sensor.py
@@ -1,33 +1,67 @@
-# custom_components/ph_sensor/sensor.py
-import homeassistant.helpers.config_validation as cv
+# ~/.esphome/components/ph_sensor/sensor.py
+import time
+import board
+import busio
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
 from homeassistant.components.sensor import PLATFORM_SCHEMA, SensorEntity
+import homeassistant.helpers.config_validation as cv
 
-PLATFORM_SCHEMA = cv.extend({
-    cv.Required('name'): cv.string,
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    cv.Required("name"): cv.string,
+    cv.Optional("temperature_sensor", default=None): cv.entity_id,
+    cv.Required("adc_channel"): cv.positive_int,
+    cv.Required("i2c_id"): cv.use_id(busio.I2C),
 })
 
+def setup_platform(hass, config, add_entities, discovery_info):
+    name = config.get("name")
+    temperature_sensor = config.get("temperature_sensor", None)
+    adc_channel = config.get("adc_channel")
+    i2c_id = config.get("i2c_id")
+
+    # Initialize the ADS1115 ADC
+    ads = ADS.ADS1115(i2c_id)
+
+    # Create an analog input channel.
+    channel = AnalogIn(ads, adc_channel)
+
+    add_entities([PhSensor(name, temperature_sensor, channel)])
+
+class PhSensor(SensorEntity):
+    def __init__(self, name, temperature_sensor, channel):
+        self._name = name
+        self._temperature_sensor = temperature_sensor
+        self._channel = channel
+        self._state = None
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def state(self):
+        return self._state
+
+    async def async_update(self):
+        # Read the raw value of the ADC channel.
+        raw_value = self._channel.value
+        # Convert the raw value to a voltage (assuming 3.3V reference).
+        voltage = raw_value * (3.3 / 65536)
+        # Calculate pH based on the voltage (this is an example calculation, you might need to adjust it).
+        pH = -2.0798 * math.log(voltage) + 12.47
+        self._state = pH
+
 def setup_platform(hass, config, add_entities, discovery_info=None):
-    sensor_name = config.get('name')
-    
-    class PhSensor(SensorEntity):
-        def __init__(self, name):
-            self._name = name
-            self._state = None
-        
-        @property
-        def name(self):
-            return self._name
-        
-        @property
-        def state(self):
-            return self._state
-        
-        @property
-        def state_class(self):
-            return 'measurement'
-        
-        @property
-        def unit_of_measurement(self):
-            return 'pH'
-    
-    add_entities([PhSensor(sensor_name)])
+    name = config.get("name")
+    temperature_sensor = config.get("temperature_sensor", None)
+    adc_channel = config.get("adc_channel")
+    i2c_id = config.get("i2c_id")
+
+    # Initialize the ADS1115 ADC
+    ads = ADS.ADS1115(i2c_id)
+
+    # Create an analog input channel.
+    channel = AnalogIn(ads, adc_channel)
+
+    add_entities([PhSensor(name, temperature_sensor, channel)])
