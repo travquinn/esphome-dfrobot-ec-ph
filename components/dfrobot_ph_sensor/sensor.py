51a6acd9addbbc8e8af74ef496e4fcf2f7ffa44e
travquinn <53845837+travquinn@users.noreply.github.com>
2024-11-15 20:18:11 +1100

    Update sensor.py

--- components/dfrobot_ph_sensor/sensor.py
+++ components/dfrobot_ph_sensor/sensor.py
@@ -1,47 +1,34 @@
-import esphome.codegen as cg
-import esphome.config_validation as cv
-from esphome.components import i2c, sensor, ads1115
-from esphome.const import CONF_ID, ICON_WATER, UNIT_PH
+# custom_components/ph_sensor/sensor.py
+import homeassistant.helpers.config_validation as cv
+from homeassistant.components.sensor import PLATFORM_SCHEMA, SensorEntity
+from .const import DOMAIN
 
-DEPENDENCIES = ["i2c"]
-CONF_TEMPERATURE_SENSOR = "temperature_sensor"
-CONF_ADC_CHANNEL = "adc_channel"
-CONF_ADS1115_ID = "ads1115_id"
+PLATFORM_SCHEMA = cv.extend({
+    cv.Required('name'): cv.string,
+})
 
-dfrobot_ph_sensor_ns = cg.esphome_ns.namespace("dfrobot_ph_sensor")
-DFRobotPHSensor = dfrobot_ph_sensor_ns.class_(
-    "DFRobotPHSensor", cg.PollingComponent, i2c.I2CDevice
-)
-
-CONFIG_SCHEMA = (
-    sensor.sensor_schema(
-        DFRobotPHSensor,
-        unit_of_measurement=UNIT_PH,
-        icon=ICON_WATER,
-        accuracy_decimals=2,
-    )
-    .extend({
-        cv.Required(CONF_TEMPERATURE_SENSOR): cv.use_id(sensor.Sensor),
-        cv.Required(CONF_ADC_CHANNEL): cv.int_range(min=0, max=3),
-        cv.Required(CONF_ADS1115_ID): cv.use_id(ads1115.ADS1115Component),  # Reference to the ADS1115 hub
-    })
-    .extend(cv.polling_component_schema("60s"))
-    .extend(i2c.i2c_device_schema(0x48))
-)
-
-async def to_code(config):
-    var = await sensor.new_sensor(config)
-    await cg.register_component(var, config)
-    await i2c.register_i2c_device(var, config)
-
-    temp_sens = await cg.get_variable(config[CONF_TEMPERATURE_SENSOR])
-    cg.add(var.set_temperature_sensor(temp_sens))
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    sensor_name = config.get('name')
     
-    adc_channel = config[CONF_ADC_CHANNEL]
-    ads_id = config[CONF_ADS1115_ID]
+    class PhSensor(SensorEntity):
+        def __init__(self, name):
+            self._name = name
+            self._state = None
+        
+        @property
+        def name(self):
+            return self._name
+        
+        @property
+        def state(self):
+            return self._state
+        
+        @property
+        def state_class(self):
+            return 'measurement'
+        
+        @property
+        def unit_of_measurement(self):
+            return 'pH'
     
-    cg.add(var.set_adc_channel(adc_channel))
-
-    # Get the ADS1115 variable and set it
-    ads_var = await cg.get_variable(ads_id)
-    cg.add(var.set_ads1115(ads_var))  # Ensure this method exists in your class definition
+    add_entities([PhSensor(sensor_name)])
