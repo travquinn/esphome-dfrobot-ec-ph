9fb3c48a4cc4363fc51c2c0b12fa175a6176805d
travquinn <53845837+travquinn@users.noreply.github.com>
2024-11-15 20:51:52 +1100

    Update dfrobot_ph_sensor.h

--- components/dfrobot_ph_sensor/dfrobot_ph_sensor.h
+++ components/dfrobot_ph_sensor/dfrobot_ph_sensor.h
@@ -1,34 +1,25 @@
-#include "dfrobot_ph_sensor.h"
-#include "esphome/core/log.h"
+// dfrobot_ph_sensor.h
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/i2c/i2c.h"
+#include "esphome/components/sensor/sensor.h"
 
 namespace esphome {
 namespace dfrobot_ph_sensor {
 
-static const char *TAG = "dfrobot_ph_sensor";
+class DFRobotPHSensor : public Component, public i2c::I2CDevice {
+ public:
+  void setup() override;
+  void update() override;
 
-void DFRobotPHSensor::setup() {
-  ph_.begin();
-  ads_->setGain(GAIN_TWOTHIRDS); // Use the pointer to access methods
-  ads_->begin();
-}
+  void set_adc_channel(uint8_t adc_channel) { adc_channel_ = adc_channel; }
+  void set_temperature_sensor(sensor::Sensor *temperature_sensor) { temperature_sensor_ = temperature_sensor; }
 
-void DFRobotPHSensor::update() {
-  if (temp_sensor_ == nullptr || !temp_sensor_->has_state()) {
-    ESP_LOGW(TAG, "Temperature sensor not available");
-    return;
-  }
-
-  float voltage = ads_->readADC_SingleEnded(adc_channel_) * 0.1875; // Adjust based on your ADC settings
-  float temperature = temp_sensor_->state;
-  float ph_value = ph_.readPH(voltage, temperature);
-
-  publish_state(ph_value);
-}
-
-void DFRobotPHSensor::dump_config() {
-  ESP_LOGCONFIG(TAG, "DFRobot pH Sensor:");
-  LOG_I2C_DEVICE(this);
-}
+ protected:
+  uint8_t adc_channel_{0};
+  sensor::Sensor *temperature_sensor_;
+};
 
 }  // namespace dfrobot_ph_sensor
 }  // namespace esphome
