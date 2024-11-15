ebe1ee0d00f1802d052f2a7ba3136376030dc659
travquinn <53845837+travquinn@users.noreply.github.com>
2024-11-15 20:24:12 +1100

    Update __init__.py

--- components/dfrobot_ph_sensor/__init__.py
+++ components/dfrobot_ph_sensor/__init__.py
@@ -1,33 +1,4 @@
-# custom_components/ph_sensor/__init__.py
-from homeassistant.components.sensor import PLATFORM_SCHEMA, SensorEntity
-import homeassistant.helpers.config_validation as cv
+# ~/.esphome/components/ph_sensor/__init__.py
+from .sensor import PhSensor
 
-PLATFORM_SCHEMA = cv.extend({
-    cv.Required('name'): cv.string,
-})
-
-def setup_platform(hass, config, add_entities, discovery_info=None):
-    sensor_name = config.get('name')
-    
-    class PhSensor(SensorEntity):
-        def __init__(self, name):
-            self._name = name
-            self._state = None
-        
-        @property
-        def name(self):
-            return self._name
-        
-        @property
-        def state(self):
-            return self._state
-        
-        @property
-        def state_class(self):
-            return 'measurement'
-        
-        @property
-        def unit_of_measurement(self):
-            return 'pH'
-    
-    add_entities([PhSensor(sensor_name)])
+__all__ = ["PhSensor"]
